91cc50beb8600644bb5f9f4ecab8933b8135f180
Frank Duncan <frank@kank.net>
2020-01-22 12:25:51 +0000

    Add torquedata to ansible.
    This involves both updating mediawiki, as well as putting the torquedata
    flask server on the target machine.

---
+++ ansible/roles/mediawiki/files/testapi.php
@@ -0,0 +1,23 @@
+<?php
+
+class TestAPI extends APIBase {
+
+  public function __construct($main, $action) {
+    parent::__construct($main, $action);
+  }
+
+  public function execute() {
+    $proposals = json_decode(file_get_contents("http://localhost:5000"));
+    $this->getResult()->addValue(null, "proposals", $proposals);
+  }
+
+  public function getAllowedParams() {
+    return [
+      "path" => "/"
+    ];
+  }
+}
+
+$wgAPIModules["torquedataconnect"] = "TestAPI";
+
+?>
