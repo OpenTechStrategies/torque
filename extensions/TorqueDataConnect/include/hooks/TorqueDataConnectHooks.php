71ef3ef16c1b5ddb96811f7ad6fbbc1926630549
Frank Duncan <frank@kank.net>
2020-01-27 12:39:30 +0000

    Add #tcdrender parser function to TorqueDataConnect

---
+++ extensions/TorqueDataConnect/include/hooks/TorqueDataConnectHooks.php
@@ -0,0 +1,19 @@
+<?php
+
+class TorqueDataConnectHooks {
+	public static function onParserFirstCallInit( Parser $parser ) {
+		$parser->setFunctionHook('tdcrender', [ self::class, 'loadLocation' ]);
+	}
+
+	public static function loadLocation($parser, $location) {
+  	$parser->disableCache();
+
+    # The combination of recursiveTagParse and isHTML set to true
+    # was the winning set of things to do to get the #evu tags from
+    # the video extension to correctly dump out html that's correctly
+    # rendered.
+    return [$parser->recursiveTagParse(file_get_contents("http://localhost:5000/" . $location)), "isHTML" => true];
+	}
+}
+
+?>
