7dd2e56bbe78d2ffed6c41670414af81528e44ee
Frank Duncan <frank@kank.net>
2020-03-28 07:53:55 -0500

    Move PickSome to its own repository
    That repository is at github.com/OpenTechStrategies/PickSome

--- extensions/PickSome/sql/picksome.sql
+++
@@ -1,8 +0,0 @@
--- MySQL/SQLite schema for the Comments extension
-  
-CREATE TABLE /*_*/PickSome (
-  id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  page_id int(11) NOT NULL default 0,
-  user_id int(11) NOT NULL default 0
-) /*$wgDBTableOptions*/;
-
