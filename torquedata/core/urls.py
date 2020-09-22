b99152f7ceb569c4ba25c112e4f898e79df06f3a
Yaxel Perez <yaxelp@gmail.com>
2020-09-21 20:28:50 -0400

    Rewrite views to use django ORM
    This commit rewrites every API endpoint except /search/<sheet_name>,
    which requires further consideration as discussed in issue #45. Input
    validation reserved for future commits; views will not fail gracefully.

---
+++ torquedata/core/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+from core import views
+
+urlpatterns = [
+    path('search/<sheet_name>', views.search),
+    path('api/<sheet_name>.<fmt>', views.get_sheet),
+    path('api/<sheet_name>/toc/<toc_name>.<fmt>', views.get_toc),
+    path('api/<sheet_name>/id/<key>.<fmt>', views.get_row),
+    path('api/<sheet_name>/attachment/<key>/<attachment>', views.get_attachment),
+    path('config/<sheet_name>/<wiki_key>/reset', views.reset_config),
+    path('config/<sheet_name>/<wiki_key>/group', views.set_group_config),
+    path('config/<sheet_name>/<wiki_key>/template', views.set_template_config),
+    path('upload/sheet', views.upload_sheet),
+    path('upload/toc', views.upload_toc),
+    path('upload/attachment', views.upload_attachment),
+    path('users/username/<username>', views.user_by_username),
+]
