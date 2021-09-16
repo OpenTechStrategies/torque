0b35aedbf6d65d3b602b38c71fc107f7b020052c
Frank Duncan <frank@kank.net>
2021-09-16 07:51:58 -0500

    Torque Wiki Configuration can now have multiple pages for columns, ids
    This required some refactoring of the parser to make it more legible, as
    it was getting too complicated if we added this feature into the
    unrolled state machine.

--- TorqueDataConnect/include/TorqueDataConnectConfig.php
+++ TorqueDataConnect/include/TorqueDataConnectConfig.php
@@ -1,37 +1,40 @@
 <?php
 class TorqueDataConnectConfig {
-  private static $errors = [];
-  private static $validTemplateTypes = ["View", "TOC", "Search", "Raw View"];
+  public static $errors = [];
 
-  public static function convertPageToFieldConfig($page) {
-    $title = Title::newFromText($page);
-    if(!$title->exists()) {
-      array_push(self::$errors, wfMessage("torquedataconnect-convert-field-page-ne", $page)->parse());
-      return [];
-    }
-    $page = new WikiPage($title);
+  public static function convertPagesToFieldConfig($pages) {
     $fields = [];
-    foreach(explode("\n", $page->getContent()->getText()) as $line) {
-      $matches = [];
-      if(preg_match("/^\* (.*)$/", $line, $matches)) {
-        $fields[] = $matches[1];
+    foreach($pages as $page) {
+      $title = Title::newFromText($page);
+      if(!$title->exists()) {
+        array_push(self::$errors, wfMessage("torquedataconnect-convert-field-page-ne", $page)->parse());
+        return [];
+      }
+      $page = new WikiPage($title);
+      foreach(explode("\n", $page->getContent()->getText()) as $line) {
+        $matches = [];
+        if(preg_match("/^\* (.*)$/", $line, $matches)) {
+          $fields[] = $matches[1];
+        }
       }
     }
     return $fields;
   }
 
-  public static function convertPageToIdConfig($page) {
-    $title = Title::newFromText($page);
-    if(!$title->exists()) {
-      array_push(self::$errors, wfMessage("torquedataconnect-convert-id-page-ne", $page)->parse());
-      return [];
-    }
-    $page = new WikiPage($title);
+  public static function convertPagesToIdConfig($pages) {
     $ids = [];
-    foreach(explode("\n", $page->getContent()->getText()) as $line) {
-      $matches = [];
-      if(preg_match("/^\* ([^:]*):.*$/", $line, $matches)) {
-        $ids[] = $matches[1];
+    foreach($pages as $page) {
+      $title = Title::newFromText($page);
+      if(!$title->exists()) {
+        array_push(self::$errors, wfMessage("torquedataconnect-convert-id-page-ne", $page)->parse());
+        return [];
+      }
+      $page = new WikiPage($title);
+      foreach(explode("\n", $page->getContent()->getText()) as $line) {
+        $matches = [];
+        if(preg_match("/^\* ([^:]*):.*$/", $line, $matches)) {
+          $ids[] = $matches[1];
+        }
       }
     }
     return $ids;
@@ -47,7 +50,7 @@ class TorqueDataConnectConfig {
     return $page->getContent()->getText();
   }
 
-  public static function commitGroupConfig($groupName, $fieldPage, $proposalPage) {
+  public static function commitGroupConfig($groupName, $fieldsPages, $proposalPages) {
     global $wgTorqueDataConnectCollectionName, $wgTorqueDataConnectWikiKey, $wgTorqueDataConnectServerLocation;
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL,
@@ -61,8 +64,8 @@ class TorqueDataConnectConfig {
       [
         "group" => $groupName,
         "wiki_key" => $wgTorqueDataConnectWikiKey,
-        "fields" => TorqueDataConnectConfig::convertPageToFieldConfig($fieldPage),
-        "valid_ids" => TorqueDataConnectConfig::convertPageToIdConfig($proposalPage)
+        "fields" => TorqueDataConnectConfig::convertPagesToFieldConfig($fieldsPages),
+        "valid_ids" => TorqueDataConnectConfig::convertPagesToIdConfig($proposalPages)
       ]));
     curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
     curl_exec ($ch);
@@ -160,96 +163,22 @@ class TorqueDataConnectConfig {
 
     $configText = (new WikiPage($configPage))->getContent()->getText();
 
-    $groupConfig = [];
-    $templateConfig = [];
-    // We have to parse this as best we can, because mediawiki doesn't really
-    // give us a mwikiText->AST that we could loop over.
-    //
-    // First pass of parsing is just unrolling the state machine out
-    // rather explicitly.  We could have a repertoire of regexes
-    // and callbacks like a normal parser, but it's currently so simple
-    // that the following is cleaner.
-    $groupName = false;
-    $fieldPage = false;
-    $idsPage = false;
-
-    $templateName = false;
-    $templatePage = false;
-    $templateType = false;
-
-    $permissionsSection = false;
-    $templatesSection = false;
+    $permissionsParser = new PermissionsSectionParser();
+    $templateParser = new TemplatesSectionParser();
+    $parser = false;
 
     foreach(explode("\n", $configText) as $line) {
       $line = trim($line);
       if(preg_match("/= *permissions *=/i", $line)) {
-        $permissionsSection = true;
-        $templatesSection = false;
+        $parser = $permissionsParser;
       } else if(preg_match("/= *templates *=/i", $line)) {
-        $templatesSection = true;
-        $permissionsSection = false;
-      } if(preg_match("/^\|\\-/", $line)) {
-        if($permissionsSection) {
-          $groupName = false;
-          $fieldPage = false;
-          $idsPage = false;
-        }else if($templatesSection) {
-          $templateName = false;
-          $templatePage = false;
-          $templateType = false;
-        }
-      } else if (preg_match("/^\|/", $line)) {
-        $line = trim(substr($line, 1));
-        if($permissionsSection) {
-          if(!$groupName) {
-            $groupName = $line;
-          } else if(!$fieldPage) {
-            $matches = [];
-            preg_match("/\\[\\[(.*)\\]\\]/", $line, $matches);
-            $fieldPage = $matches[1];
-          } else if(!$idsPage) {
-            $matches = [];
-            preg_match("/\\[\\[(.*)\\]\\]/", $line, $matches);
-            $idsPage = $matches[1];
-          } else {
-            // Do nothing here, since a fourth field is ok for user notes if they like
-          }
-          if($groupName && $fieldPage && $idsPage) {
-            array_push($groupConfig, [
-              "groupName" => $groupName,
-              "fieldPage" => $fieldPage,
-              "idsPage" => $idsPage
-            ]);
-          }
-        } else if($templatesSection) {
-          if(!$templateName) {
-            $templateName = $line;
-          } else if(!$templatePage) {
-            $matches = [];
-            if(preg_match("/\\[\\[(.*)\\]\\]/", $line, $matches)) {
-              $templatePage = $matches[1];
-            }
-          } else if(!$templateType) {
-            $templateType = $line;
-
-            if(in_array($templateType, self::$validTemplateTypes)) {
-              array_push($templateConfig, [
-                "templateName" => $templateName,
-                "templatePage" => $templatePage,
-                "templateType" => $templateType
-              ]);
-            } else {
-              array_push(self::$errors, wfMessage("torquedataconnect-config-it", $templateType)->parse());
-            }
-          } else {
-            // Do nothing here, since a fourth field is ok for user notes if they like
-          }
-        }
+        $parser = $templateParser;
+      } else if($parser) {
+        $parser->parseLine($line);
       }
-
     }
 
-    return [$groupConfig, $templateConfig];
+    return [$permissionsParser->getConfig(), $templateParser->getConfig()];
   }
 
   public static function commitConfigToTorqueData() {
@@ -264,8 +193,8 @@ class TorqueDataConnectConfig {
     foreach($groupConfig as $group) {
       TorqueDataConnectConfig::commitGroupConfig(
         $group["groupName"],
-        $group["fieldPage"],
-        $group["idsPage"]
+        $group["fieldsPages"],
+        $group["idsPages"]
       );
     }
     foreach($templateConfig as $template) {
@@ -286,8 +215,8 @@ class TorqueDataConnectConfig {
 
     [$groupConfig, $templateConfig] = TorqueDataConnectConfig::parseConfig();
     foreach($groupConfig as $config) {
-      if($title->equals(Title::newFromText($config["fieldPage"])) ||
-         $title->equals(Title::newFromText($config["idsPage"]))) {
+      if($title->equals(Title::newFromText($config["fieldsPages"])) ||
+         $title->equals(Title::newFromText($config["idsPages"]))) {
         return true;
       }
     }
@@ -304,8 +233,8 @@ class TorqueDataConnectConfig {
     self::$errors = [];
     [$groupConfig, $templateConfig] = TorqueDataConnectConfig::parseConfig();
     foreach($groupConfig as $group) {
-      TorqueDataConnectConfig::convertPageToFieldConfig($group["fieldPage"]);
-      TorqueDataConnectConfig::convertPageToIdConfig($group["idsPage"]);
+      TorqueDataConnectConfig::convertPagesToFieldConfig($group["fieldsPages"]);
+      TorqueDataConnectConfig::convertPagesToIdConfig($group["idsPages"]);
     }
     return self::$errors;
   }
@@ -354,4 +283,102 @@ class TorqueDataConnectConfig {
   }
 }
 
+class PermissionsSectionParser {
+  private $groupName = false;
+  private $fieldPages = [];
+  private $idsPages = [];
+  private $groupConfig = [];
+  private $cell = "";
+
+  public function parseLine($line) {
+    if (preg_match("/^\|/", $line)) {
+      if($this->cell) {
+        if(!$this->groupName) {
+          $this->groupName = $this->cell;
+        } else if(!$this->fieldPages) {
+          foreach(explode("\n", $this->cell) as $potential) {
+            $matches = [];
+            if(preg_match("/\\[\\[(.*)\\]\\]/", $potential, $matches)) {
+              array_push($this->fieldPages, $matches[1]);
+            }
+          }
+        } else if(!$this->idsPages) {
+          foreach(explode("\n", $this->cell) as $potential) {
+            $matches = [];
+            if(preg_match("/\\[\\[(.*)\\]\\]/", $potential, $matches)) {
+              array_push($this->idsPages, $matches[1]);
+            }
+          }
+        } else {
+          // Do nothing here, since a fourth field is ok for user notes if they like
+        }
+
+        if($this->groupName && $this->fieldPages && $this->idsPages) {
+          array_push($this->groupConfig, [
+            "groupName" => $this->groupName,
+            "fieldsPages" => $this->fieldPages,
+            "idsPages" => $this->idsPages
+          ]);
+        }
+      }
+
+      $this->cell = trim(substr($line, 1));
+
+      if(preg_match("/^\|\\-/", $line)) {
+        $this->groupName = false;
+        $this->fieldPages = [];
+        $this->idsPages = [];
+        $this->cell = "";
+      }
+    } else {
+      $this->cell .= "\n" . $line;
+    }
+  }
+
+  public function getConfig() {
+    return $this->groupConfig;
+  }
+}
+
+class TemplatesSectionParser {
+  private $templateName = false;
+  private $templatePage = false;
+  private $templateType = false;
+  private $templateConfig = [];
+  private static $validTemplateTypes = ["View", "TOC", "Search", "Raw View"];
+
+  public function parseLine($line) {
+    if(preg_match("/^\|\\-/", $line)) {
+      $this->templateName = false;
+      $this->templatePage = false;
+      $this->templateType = false;
+    } else if (preg_match("/^\|/", $line)) {
+      $line = trim(substr($line, 1));
+      if(!$this->templateName) {
+        $this->templateName = $line;
+      } else if(!$this->templatePage) {
+        $matches = [];
+        if(preg_match("/\\[\\[(.*)\\]\\]/", $line, $matches)) {
+          $this->templatePage = $matches[1];
+        }
+      } else if(!$this->templateType) {
+        $this->templateType = $line;
+
+        if(in_array($this->templateType, self::$validTemplateTypes)) {
+          array_push($this->templateConfig, [
+            "templateName" => $this->templateName,
+            "templatePage" => $this->templatePage,
+            "templateType" => $this->templateType
+          ]);
+        } else {
+          array_push(TorqueDataConnectConfig::$errors, wfMessage("torquedataconnect-config-it", $templateType)->parse());
+        }
+      }
+    }
+  }
+
+  public function getConfig() {
+    return $this->templateConfig;
+  }
+}
 ?>
